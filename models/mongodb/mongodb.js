e3f3978476658865ada42a6325280da52a80bc8c
hoangi19 <viethoanghy99@gmail.com>
2021-06-09 15:16:46 +0700

    mongodb model

---
+++ models/mongodb/mongodb.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient
+
+const url = 'mongodb://admin:admin@127.0.0.1:27017'
+
+const client = new MongoClient(url)
+
+// connect mongodb
+async function connectDB(){
+    return client.connect()        
+}
+
+// disconnect mongodb
+async function disconnectDB(){
+    return client.close()
+}
+
+// update bitx_eth_price_5m
+function updateBITXETHprice5m(data, callbackFunc) {
+    const db = client.db('test')
+    db.collection('bitx_eth_price_5m').insertOne(data, callbackFunc)
+}
+
+// get bitx_eth_price_5m value
+function getBITXETHprice5m(callbackFunc){
+    const db = client.db('test')
+    db.collection('bitx_eth_price_5m').find({}).toArray(callbackFunc)
+}
+
+module.exports = {
+    connectDB,
+    disconnectDB,
+    updateBITXETHprice5m,
+    getBITXETHprice5m
+}
